cd062c914ada1140c5eef17fbcb030120459eb7e
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2016-07-08 23:32:20 -0500

    refactor numeric values

--- machine learning 4.py
+++ machine learning 4.py
@@ -29,37 +29,37 @@ LA4270      1798    IC
 def translate ( value ):
     print value, #the ',' stops the jump of the line in python
     if value == 0:
-        print "T-PNP oscillator\n" 
-    elif value == 1:
         print "IC: integrated circuit\n"
+    elif value == 1:
+        print "T-PNP oscillator\n" 
     return
 
 # features = [[ 0 = LA | 1 = T-PNP , part]]
-#labels = 0 = other | 1 = IC #
+#labels = 0 = IC | 1 = other #
 features = [[0, 4140]]
-labels = [1]#
+labels = [0]#
 features.append([0, 4175])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4170])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4175])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4185])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4192])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4201])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4260])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4261])
-labels.append(1)#
+labels.append(0)#
 features.append([0, 4270])
-labels.append(1)#
+labels.append(0)#
 
 
 features.append([1, 27]) 
-labels.append(0)#
+labels.append(1)#
 
 #create DecisionTree
 clf = tree.DecisionTreeClassifier()
